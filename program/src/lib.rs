131f07f157a1492fedc99c7b987eb97debea66a8
phillong <phillong@localhost.localdomain>
2024-02-22 13:37:33 +0700

    first commit

---
+++ program/src/lib.rs
@@ -0,0 +1,23 @@
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    msg,
+};
+
+// declare and export the program's entrypoint
+entrypoint!(hello_world);
+
+// program entrypoint's implementation
+pub fn hello_world(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]
+) -> ProgramResult {
+    // log a message to the blockchain
+    msg!("Hello, world!");
+
+    // exit the program
+    Ok(())
+}
